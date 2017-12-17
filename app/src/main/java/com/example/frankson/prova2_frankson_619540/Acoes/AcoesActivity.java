87a22180376567428c9c705ac565a526b8d7d8e1
frankson <frankson100014@gmail.com>
2017-12-17 15:25:14 -0200

    Detalhes finalizado

--- app/src/main/java/com/example/frankson/prova2_frankson_619540/Acoes/AcoesActivity.java
+++ app/src/main/java/com/example/frankson/prova2_frankson_619540/Acoes/AcoesActivity.java
@@ -1,5 +1,6 @@
 package com.example.frankson.prova2_frankson_619540.Acoes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.example.frankson.prova2_frankson_619540.AcoesDetalhes.AcoesDetailActivity;
 import com.example.frankson.prova2_frankson_619540.R;
 import com.example.frankson.prova2_frankson_619540.entity.AcaoEntity;
 
@@ -44,14 +46,23 @@ public class AcoesActivity extends AppCompatActivity implements AcoesView{
     }
 
     @Override
-    public void setList(List<AcaoEntity> acoesList) {
+    public void setList(final List<AcaoEntity> acoesList) {
 
         AcoesAdapter acoesAdapter = new AcoesAdapter(acoesList ,this);
 
         acoesAdapter.setOnRecyclerViewSelected(new OnRecyclerViewSelected() {
             @Override
             public void onClick(View view, int position) {
-                Toast.makeText(AcoesActivity.this, "Clique rápido", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent
+                        (AcoesActivity.this,
+                                AcoesDetailActivity.class);
+
+                intent.putExtra("Name", acoesList.get(position).getName());
+                intent.putExtra("Image", acoesList.get(position).getImage());
+                intent.putExtra("Description", acoesList.get(position).getDescription());
+                intent.putExtra("Site", acoesList.get(position).getSite());
+                startActivity(intent);
+                //Toast.makeText(AcoesActivity.this, "Clique rápido", Toast.LENGTH_SHORT).show();
             }
 
         });
---
+++ app/src/main/java/com/example/frankson/prova2_frankson_619540/AcoesDetalhes/AcoesDetailActivity.java
@@ -0,0 +1,53 @@
+package com.example.frankson.prova2_frankson_619540.AcoesDetalhes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.frankson.prova2_frankson_619540.R;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AcoesDetailActivity extends AppCompatActivity {
+    @BindView(R.id.text_view_description)
+    TextView txDescricao;
+
+    @BindView(R.id.text_view_site)
+    TextView txSite;
+
+    @BindView(R.id.image_view_header)
+    ImageView imgHead;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acoes_detail);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("Name");
+        String image = intent.getStringExtra("Image");
+        String descricao = intent.getStringExtra("Description");
+        String site = intent.getStringExtra("Site");
+
+
+
+        //Altera o titulo da action bar ActionBar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(name);
+        }
+
+        txDescricao.setText(descricao);
+        txSite.setText(site);
+        Picasso.with(getBaseContext())
+                .load(image)
+                .centerCrop()
+                .fit()
+                .into(imgHead);
+    }
+
+}
